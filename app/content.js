9a545b21f2a0a50a170ca49ba971dd1a0cd9bde4
Fortyseven <fortyseven@gmail.com>
2015-07-04 17:02:36 -0400

    Added 'questionable' website scan

--- app/content.js
+++ app/content.js
@@ -13,40 +13,70 @@ const SATIRE_DOMAINS = [
     "dailymediabuzz.com", "demyx.com", "americannews.com", "newslo.com"
 ];
 
+const QUESTIONABLE_DOMAINS = [
+    "infowars.com", "prisonplanet.com", "prisonplanet.tv", "in5d.com", "whatreallyhappened.com",
+    "abovetopsecret.com", "wanttoknow.info", "beforeitsnews.com", "cabaltimes.com", "rense.com", "crossroad.to",
+    "alternet.org", "watch-unto-prayer.org", "madcowprod.com", "globalresearch.ca", ".rt.com", "presstv.ir",
+    "barrychamish.com", "godlikeproductions.com", "stj911.org", "goroadachi.com", "henrymakow.com",
+    "educate-yourself.org", "letsrollforums.com", "vigilantcitizen.com", "voltairenet.org", "larouchepub.com",
+    "theblaze.com", "septemberclues.info", "thetruthseeker.co.uk", "mufon.com", "mufoncanada.com", "openminds.tv",
+    "drudgereport.com", "naturalnews.com", "coasttocoastam.com", "davidicke.com", "activistpost.com",
+    "stevequayle.com", "shtfplan.com", "thedailysheeple.com", "informationclearinghouse.info",
+    "americanfreepress.net", "antiwar.com", "truthstreammedia.com", "thenewamerican.com",
+    "tarpley.net", "911truth.org", "wnd.com", "propagandamatrix.com", "oathkeepers.org", "cryptogon.com",
+    "alt-market.com", "legitgov.org", "whatdoesitmean.com", "ronpaulinstitute.org", "tvnewslies.org",
+    "dprogram.net", "smoloko.com", "themindunleashed.org", "waykiwayki.com", "disclose.tv",
+    "mirror.co.uk", "dailystar.co.uk", "dailymail.co.uk", "thesun.co.uk", "mercola.com", "agoracosmopolitan.com",
+    "cryptozoologynews.com", "newshounds.us", "topekasnews.com", "pravda.ru", "eutimes.net",
+    "examiner.com", "theepochtimes.com", "yournewswire.com", "worldtruth.tv", "wakeup-world.com",
+    "ewao.com", "ufointernationalproject.com", "spiritlibrary.com", "theinsider.org", "collective-evolution.com",
+    "ufosonline.blogspot.com.br", "ufocasebook.com", "exopolitics.org", "doctoroz.com", "drphil.com",
+    "conspiracytheorist.com.au", "ijreview.com", "thefreethoughtproject.com",
+];
+
 const REFRESH_DELAY = 750;
 
-$().ready(function (){
-    parsePage();
-});
+$().ready( function ()
+           {
+               parsePage();
+           } );
 
 function parsePage()
 {
-    $(ARTICLE_CLASS).each(
-        function (index)
+    $( ARTICLE_CLASS ).each(
+        function ()
         {
-            var self = $(this);
-            var href = $(ARTICLE_LINK_SELECTOR, self).attr("onmouseover") || null;
-            if (href != null)
-            {
-                href = href.toLowerCase();
-                SATIRE_DOMAINS.forEach(
-                    function (element, index, array)
-                    {
-                        if (href.indexOf(element) >= 0)
+            var self = $( this );
+            var href = $( ARTICLE_LINK_SELECTOR, self ).attr( "onmouseover" ) || null;
+            if ( href != null ) {
+                parse( SATIRE_DOMAINS, "satireItem", href );
+                parse( QUESTIONABLE_DOMAINS, "questionableItem", href );
+
+                //--------------------------------
+                function parse( source_elements, marking_class, source_url )
+                {
+                    source_url = source_url.toLowerCase();
+                    source_elements.forEach(
+                        function ( element, index, array )
                         {
-                            self.addClass("satireItem");
-//                        var title = $("h5 a :first", self);
-//                        if (title && !title.data("modified")) {
-//                            title.html( title.html() + "<span class=\"satire_label\">* Satire</span>");
-//                            title.data("modified", true);
-//                        }
-                            return false;
-                        }
-                        return true;
-                    });
+                            if ( source_url.indexOf( element ) >= 0 ) {
+                                self.addClass( marking_class );
+//                                console.log(marking_class + " type MATCHED", element);
+//                                var title = $("h5 a :first", self);
+//                                if (title && !title.data("modified")) {
+//                                   title.html( title.html() + "<span class=\"satire_label\">* Satire</span>");
+//                                   title.data("modified", true);
+//                                }
+                                return false;
+                            }
+                            return true;
+                        } );
+                }
+
+                //--------------------------------
             }
         }
     );
 
-    setTimeout(parsePage, REFRESH_DELAY);
+    setTimeout( parsePage, REFRESH_DELAY );
 }
