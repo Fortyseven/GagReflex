deb565d4e3918bf5a410127b89d23de12463daa6
Fortyseven <fortyseven@gmail.com>
2015-11-01 00:34:35 -0400

    First round of Reddit support; encapsulated rendering portion of gag.js into gag_renderers

--- app/gag.js
+++ app/gag.js
@@ -8,73 +8,74 @@ var Gag = function ( config )
 {
 //    this.self = this;
 
-    this.ARTICLE_CLASS = "div[aria-label=\"Story\"]";
-    this.ARTICLE_LINK_SELECTOR = "a._6kt, a._52c6, div._6m6>a, userContent a";
-
-    this.REFRESH_DELAY = 1000;
-
+//    this.ARTICLE_CLASS = "div[aria-label=\"Story\"]";
+//    this.ARTICLE_LINK_SELECTOR = "a._6kt, a._52c6, div._6m6>a, userContent a";
+//
+//    this.REFRESH_DELAY = 1000;
+//
+    this._renderer = null;
     this._config = config;
-
-    /***************************************
-     * Callback to do highlighting on page
-     */
-    this.runHighlighting = function ()
-    {
-        var gag = this;
-        $( gag.ARTICLE_CLASS ).each(
-                function ()
-                {
-                    var href = $( gag.ARTICLE_LINK_SELECTOR, this ).attr( "onmouseover" ) || null;
-
-                    if ( href != null ) {
-                        for ( var gi in gag._config.groups ) {
-                            if ( gag._config.groups[gi].is_enabled ) {
-                                if ( parse( gag._config.groups[gi], href, this ) ) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-        );
-        setTimeout( this.runHighlighting.bind( this ), this.REFRESH_DELAY );
-    };
+//
+//    /***************************************
+//     * Callback to do highlighting on page
+//     */
+//    this.runHighlighting = function ()
+//    {
+//        var gag = this;
+//        $( gag.ARTICLE_CLASS ).each(
+//                function ()
+//                {
+//                    var href = $( gag.ARTICLE_LINK_SELECTOR, this ).attr( "onmouseover" ) || null;
+//
+//                    if ( href != null ) {
+//                        for ( var gi in gag._config.groups ) {
+//                            if ( gag._config.groups[gi].is_enabled ) {
+//                                if ( parse( gag._config.groups[gi], href, this ) ) {
+//                                    break;
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//        );
+//        setTimeout( this.runHighlighting.bind( this ), this.REFRESH_DELAY );
+//    };
+//
+//    /**
+//     * Scans a string for domains from suppled group definition and
+//     * applies CSS rule to a given element if found.
+//     *
+//     * @param group_def Group definition object
+//     * @param source_url String to scan for domains
+//     * @param parent_element Element to add CSS class to if matched
+//     * @return {boolean} True if there was a match
+//     */
+//    var parse = function ( group_def, source_url, parent_element )
+//    {
+//        var has_found_match = false;
+//
+//        group_def.domains.forEach(
+//                function ( element, index, array )
+//                {
+//                    // Does the domain match somewhere in the URL? // FIXME
+//                    if ( source_url.indexOf( element ) > -1 ) {
+//
+//                        // Brand it with the class belonging to the group
+//                        parent_element.classList.add( group_def.css_name );
+//                        has_found_match = true;
+//                        return true;
+//                    }
+//                    return false;
+//                }
+//        );
+//
+//        return has_found_match;
+//    };
 
     /**
-     * Scans a string for domains from suppled group definition and
-     * applies CSS rule to a given element if found.
-     *
-     * @param group_def Group definition object
-     * @param source_url String to scan for domains
-     * @param parent_element Element to add CSS class to if matched
-     * @return {boolean} True if there was a match
-     */
-    var parse = function ( group_def, source_url, parent_element )
-    {
-        var has_found_match = false;
-
-        group_def.domains.forEach(
-                function ( element, index, array )
-                {
-                    // Does the domain match somewhere in the URL? // FIXME
-                    if ( source_url.indexOf( element ) > -1 ) {
-
-                        // Brand it with the class belonging to the group
-                        parent_element.classList.add( group_def.css_name );
-                        has_found_match = true;
-                        return true;
-                    }
-                    return false;
-                }
-        );
-
-        return has_found_match;
-    };
-
-    /**
      *
      */
-    this.rebuildCSSRules = function (config)
+    this.rebuildCSSRules = function ( config )
     {
         if ( !config ) {
             throw "Invalid or undefined gag reflex config";
@@ -98,9 +99,28 @@ var Gag = function ( config )
     /********************************************************
      * Begin processing the current page
      */
-    this.run = function(config)
+    this.run = function ()
     {
-        this.rebuildCSSRules(this._config);
-        setTimeout( this.runHighlighting.bind( this ), this.REFRESH_DELAY );
+        this.rebuildCSSRules( this._config );
+
+        var site = document.location.hostname;
+
+        if ( site.indexOf( "facebook.com" ) > -1 ) {
+            this._renderer = new GagFacebook( this._config );
+        }
+        else if ( site.indexOf( "reddit.com" ) > -1 ) {
+            this._renderer = new GagReddit( this._config );
+        }
+        else {
+            throw "Could not find proper renderer for '" + site + "'";
+            return;
+        }
+
+        // via http://stackoverflow.com/questions/280634/endswith-in-javascript
+        String.prototype.endsWith = function ( suffix ) {
+            return this.indexOf( suffix, this.length - suffix.length ) !== -1;
+        };
+
+        this._renderer.start();
     };
 };
