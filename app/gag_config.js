3a259bc24f56aee20e4577651d3fb86a1a5955e6
Fortyseven <fortyseven@gmail.com>
2015-11-01 22:25:02 -0500

    Added another questionable; cruft ejection

--- app/gag_config.js
+++ app/gag_config.js
@@ -68,12 +68,11 @@ var GagConfig = function ( on_loading_complete_callback )
         "philosophers-stone.co.uk", "kendoc911.files.wordpress.com", "freerepublic.com", "sott.net",
         "wattsupwiththat.com", "subvertednation.net", "jewishcrimenetworkdid911.blogspot.com.au",
         "seeker401.wordpress.com", "revelation13.net", "gotnews.com", "reason.com", "thegatewaypundit.com",
-        "tvoinews.com", "rightwingwatch.org", "boilingfrogspost.com", "allnewspipeline.com", "greenlifes.net"
+        "tvoinews.com", "rightwingwatch.org", "boilingfrogspost.com", "allnewspipeline.com", "greenlifes.net",
+        "therealnews.com"
 
         // If Drudge Report actually hosted its own original content, rest assured, a link to the site would go here.
     ];
-
-    //this._config = {};
 
     this.loadConfig( on_loading_complete_callback );
 };
